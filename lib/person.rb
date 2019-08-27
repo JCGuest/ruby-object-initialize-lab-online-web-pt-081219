9568b28559352fb57c12e475dec3739b962e6f39
John Guest <jcguest90@gmail.com>
2019-08-27 17:25:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -25,21 +25,3 @@ end
 def name
   @name 
 end  
-end
-
-class Dog
-
-def initialize(dog_name)
-  @name = dog_name
-end
-
-def name=(dog_name)
-   @name = dog_name
-end 
- 
-def name
-  @name
-end
-
-
-end 
