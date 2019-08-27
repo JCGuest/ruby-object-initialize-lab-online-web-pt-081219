effc8b96fd53aa5010532d4d112c3c4d58586582
John Guest <jcguest90@gmail.com>
2019-08-27 17:19:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -14,7 +14,15 @@
 
 class Person
 
-attr_accessor :name
+def initialize(person_name)
+  @name = person_name
+end 
 
+def name=(person_name)
+  @name = person_name
+end 
 
+def name
+  @name 
+end  
 end
